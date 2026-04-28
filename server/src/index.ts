23592e8e419eb7fa2088f4bc3ff82c3cd29c43ad
aliftareq <aliftareq@gmail.com>
2026-04-28 11:39:33 +0600

    made api router and bussiness logic for dashboard metrics

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,9 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 
+/* ROUTE IMPORTS */
+import dashboardRoutes from "./routes/dashboardRoutes";
+
 
 
 /* CONFIGURATIONS */
@@ -17,6 +20,10 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+
+/* ROUTES */
+app.use("/dashboard", dashboardRoutes); // http://localhost:8000/dashboard
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3001;
